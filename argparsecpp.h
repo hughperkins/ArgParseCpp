b8340916f5c066382fa382cedf9ca69184580b85
Hugh Perkins <hughperkins@gmail.com>
2016-11-07 11:25:34 +0000

    reindent

--- argparsecpp.h
+++ argparsecpp.h
@@ -24,58 +24,61 @@
 
 
 namespace argparsecpp {
-    class Option {
-    public:
-        virtual  bool needsValue() = 0;
-        virtual void parse(std::string valueAsString = "") = 0;
-        Option *required() {
-            this->_required = true;
-            return this;
-        }
-        Option *help(std::string help) {
-            this->_help = help;
-            return this;
-        }
-        bool _required = false;
-        std::string _help = "";
-    };
-    class OptionBool : public Option {
-    public:
-        OptionBool(bool *target);
-        bool *target;
-        virtual  bool needsValue();
-        void parse(std::string valueString);
-    };
-    class OptionString : public Option {
-    public:
-        OptionString(std::string *target);
-        std::string *target;
-        virtual  bool needsValue();
-        void parse(std::string valueString);
-    };
-    class OptionFloat : public Option {
-    public:
-        OptionFloat(float *target);
-        float *target;
-        virtual  bool needsValue();
-        void parse(std::string valueString);
-    };
-    class OptionInt : public Option {
-    public:
-        OptionInt(int *target);
-        int *target;
-        virtual  bool needsValue();
-        void parse(std::string valueString);
-    };
 
-    class ArgumentParser {
-    public:
-        std::map<std::string, std::unique_ptr<Option> > options;
-        Option *add_int_argument(std::string option, int *var);
-        Option *add_bool_argument(std::string option, bool *var);
-        Option *add_float_argument(std::string option, float *var);
-        Option *add_string_argument(std::string option, std::string *var);
-        bool parse_args(int argc, char *argv[]);
-        void print_usage();
-    };
-}
+class Option {
+public:
+    virtual  bool needsValue() = 0;
+    virtual void parse(std::string valueAsString = "") = 0;
+    Option *required() {
+        this->_required = true;
+        return this;
+    }
+    Option *help(std::string help) {
+        this->_help = help;
+        return this;
+    }
+    bool _required = false;
+    std::string _help = "";
+};
+class OptionBool : public Option {
+public:
+    OptionBool(bool *target);
+    bool *target;
+    virtual  bool needsValue();
+    void parse(std::string valueString);
+    // void defaultTrue();
+};
+class OptionString : public Option {
+public:
+    OptionString(std::string *target);
+    std::string *target;
+    virtual  bool needsValue();
+    void parse(std::string valueString);
+};
+class OptionFloat : public Option {
+public:
+    OptionFloat(float *target);
+    float *target;
+    virtual  bool needsValue();
+    void parse(std::string valueString);
+};
+class OptionInt : public Option {
+public:
+    OptionInt(int *target);
+    int *target;
+    virtual  bool needsValue();
+    void parse(std::string valueString);
+};
+
+class ArgumentParser {
+public:
+    std::map<std::string, std::unique_ptr<Option> > options;
+    Option *add_int_argument(std::string option, int *var);
+    Option *add_bool_argument(std::string option, bool *var);
+    Option *add_float_argument(std::string option, float *var);
+    Option *add_string_argument(std::string option, std::string *var);
+    bool parse_args(int argc, char *argv[]);
+    void print_usage();
+};
+
+} // namespace argparsecpp
